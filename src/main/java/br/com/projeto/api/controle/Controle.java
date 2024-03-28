fc67461f0530aa944187ebef31852cfa12c7ce7d
Thiago11112000 <70289167+Thiago11112000@users.noreply.github.com>
2024-03-28 03:34:11 -0300

    Inicializando projeto e integrando com o postgresql

---
+++ src/main/java/br/com/projeto/api/controle/Controle.java
@@ -0,0 +1,15 @@
+package br.com.projeto.api.controle;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+
+public class Controle {
+    @GetMapping("/")
+    public  String teste(){
+        return "Hello World!";
+    }
+
+}
