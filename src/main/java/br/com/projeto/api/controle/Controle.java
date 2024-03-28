f820a4adda11378f5f61d86ea99eb6895607e482
Thiago11112000 <70289167+Thiago11112000@users.noreply.github.com>
2024-03-28 15:24:38 -0300

    Rota de remoção

--- src/main/java/br/com/projeto/api/controle/Controle.java
+++ src/main/java/br/com/projeto/api/controle/Controle.java
@@ -27,4 +27,8 @@ public class Controle {
         return acao.save(c);
     }
 
+    @DeleteMapping("/{codigo}")
+    public void remover( @PathVariable Long codigo){
+      acao.deleteById(codigo);
+    }
 }
